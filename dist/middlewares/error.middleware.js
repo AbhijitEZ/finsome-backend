0602ab2178ab0de61396e50a827ecfeb614f8425
abhijitez <abhijit.nair659@gmail.com>
2022-03-29 19:13:32 +0530

    REFACTOR: build

--- dist/middlewares/error.middleware.js
+++ dist/middlewares/error.middleware.js
@@ -5,7 +5,7 @@ const errorMiddleware = (error, req, res, next) => {
     var _a;
     try {
         // @ts-ignore
-        const status = error.status || error.code === 'LIMIT_FILE_SIZE' ? 400 : 500;
+        const status = (error === null || error === void 0 ? void 0 : error.code) === 'LIMIT_FILE_SIZE' ? 400 : error.status ? error.status : 500;
         const message = error.message || 'Something went wrong';
         const data = (_a = error.data) !== null && _a !== void 0 ? _a : {};
         logger_1.logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
